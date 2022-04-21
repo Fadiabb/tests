5d1ecc4ff59f27933a6c29ddb822d4dbf19dada5
Fadi Abbud <39081670+Fadiabb@users.noreply.github.com>
2022-04-21 09:41:46 +0200

    Update install.sh

--- docs/install.sh
+++ docs/install.sh
@@ -4,6 +4,3 @@ set -e
 
 sudo apt install -y make bash sed tar git nginx fcgiwrap gnutls-bin
 
-# Install Go from binary distribution
-curl -O https://storage.googleapis.com/golang/go1.18.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.18.linux-amd64.tar.gz
