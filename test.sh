0716ff1c4bd389860e40e89e4aa156245e753e0f
Fadi Abbud <39081670+Fadiabb@users.noreply.github.com>
2022-04-26 10:47:10 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ sudo mkdir -p /etc/nginx/sites-available
 touch default
 sudo cp default $NGINX_CONFIG_PATH
 sudo cp $NGINX_CONFIG_PATH $NGINX_CONFIG_PATH.org
-ls $NGINX_CONFIG_PATH 
+cd  $NGINX_CONFIG_PATH 
+ls -la
+id
