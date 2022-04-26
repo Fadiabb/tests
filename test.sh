b64acd40d360cf8de84deb6cbea510c86acb1fe6
Fadi Abbud <39081670+Fadiabb@users.noreply.github.com>
2022-04-26 09:50:59 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-
-sudo mkdir -p /etc/nginx
-touch test.org
-sudo cp test.org /etc/nginx
-cd /etc/nginx
-echo "pdw=" $PWD
-ls
-
+NGINX_CONFIG_PATH=/etc/nginx/sites-available/default
+sudo mkdir -p /etc/nginx/sites-available
+touch default
+sudo cp default $NGINX_CONFIG_PATH
+sudo cp $NGINX_CONFIG_PATH $NGINX_CONFIG_PATH.org
